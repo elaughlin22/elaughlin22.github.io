a04c1cdcc1bdd95306c600aee0100ecccc17383a
rlaughlin22 <47514076+rlaughlin22@users.noreply.github.com>
2019-08-08 18:25:55 -0400

    Updated schedule for 2019-2020 school year

--- day-counter/js/days.js
+++ day-counter/js/days.js
@@ -6,9 +6,9 @@ var days = [
 		"day": 1,
 		"periods": [
 			{"period": 1, "length": 40, "start": "8:15"},
-			{"period": "prt", "length": 20, "start": "8:55"},
-			{"period": 2, "length": 45, "start": "9:15"},
-			{"period": 3, "length": 60, "start": "10:00"},
+			{"period": "prt", "length": 35, "start": "8:55"},
+			{"period": 2, "length": 45, "start": "9:30"},
+			{"period": 3, "length": 45, "start": "10:15"},
 			{"period": 4, "length": 45, "start": "11:00"},
 			{"period": 5, "length": 40, "start": "11:45"},
 			{"period": "prt", "length": 25, "start": "12:25"},
